3394889ae67baef654e0131c6cb83d35687c32c9
ASSET <asset@MacBook-Pro-ASSET.local>
2024-07-20 17:47:16 +0200

    4first commit

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,13 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  build: {
+    outDir: 'dist'
+  },
+  server: {
+    port: 3000
+  },
+  preview: {
+    port: 10000
+  }
 });
